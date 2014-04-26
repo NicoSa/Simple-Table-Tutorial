5cb504fbb55acdcecceff03519625f52626157b2
Nico Saueressig <nico@nicosaueressig.de>
2014-04-26 14:00:44 +0100

    playing around, almost finished

---
+++ Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/Simple Table/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Simple Table
+//
+//  Created by Nico Saueressig on 26/04/2014.
+//  Copyright (c) 2014 Nico Saueressig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SimpleTableAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SimpleTableAppDelegate class]));
+    }
+}
--- Simple Table/SimpleTableViewController.m
+++ Simple Table/SimpleTableViewController.m
@@ -13,13 +13,34 @@
 @end
 
 @implementation SimpleTableViewController
+{
+    NSArray *recipes;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    recipes = @[@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger",@"Ham and Egg Sandwich", @"Crème Brûlée", @"White Chococate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle"];
 }
-
+-(NSInteger)tableView:(UITableView*)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"creme_brelee.jpg"];
+    return cell;
+}
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
